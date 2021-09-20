d97f0722c86a68ab2f4eca42c8e2fb38a5e6334b
Marco Toledo <marcoantonioribeirodetoledo@gmail.com>
2021-09-20 14:25:08 -0300

    Update

--- init.el
+++ init.el
@@ -17,18 +17,18 @@
 ;; == GLOBAL ==
 
 ;; == Ispell ==
-(with-eval-after-load "ispell"
-  ;; Configure `LANG`, otherwise ispell.el cannot find a 'default
-  ;; dictionary' even though multiple dictionaries will be configured
-  ;; in next line.
-  (setenv "LANG" "en_US.UTF-8")
-  (setq ispell-program-name "hunspell")
-  ;; Configure English and Portuguese.
-  (setq ispell-dictionary "pt_BR,en_US")
-  ;; ispell-set-spellchecker-params has to be called
-  ;; before ispell-hunspell-add-multi-dic will work
-  (ispell-set-spellchecker-params)
-  (ispell-hunspell-add-multi-dic "pt_BR,en_US"))
+;; (with-eval-after-load "ispell"
+;;   ;; Configure `LANG`, otherwise ispell.el cannot find a 'default
+;;   ;; dictionary' even though multiple dictionaries will be configured
+;;   ;; in next line.
+;;   (setenv "LANG" "en_US.UTF-8")
+;;   (setq ispell-program-name "hunspell")
+;;   ;; Configure English and Portuguese.
+;;   (setq ispell-dictionary "pt_BR,en_US")
+;;   ;; ispell-set-spellchecker-params has to be called
+;;   ;; before ispell-hunspell-add-multi-dic will work
+;;   (ispell-set-spellchecker-params)
+;;   (ispell-hunspell-add-multi-dic "pt_BR,en_US"))
 
 ;; The personal dictionary file has to exist, otherwise hunspell will
 ;; silently not use it.
