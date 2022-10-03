480f50ffb139f04e3666895dab61eb14485150ac
Marco Toledo <marcoantonioribeirodetoledo@gmail.com>
2022-10-03 15:57:21 -0300

    feature: Main

---
+++ custom.el
@@ -0,0 +1,42 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(LaTeX-item-indent 0)
+ '(LaTeX-label-function 'reftex-label)
+ '(auto-image-file-mode t)
+ '(bidi-paragraph-direction 'left-to-right)
+ '(company-meghanada-insert-args t)
+ '(fancy-splash-image
+   "/home/ocramoi/.emacs.d/modules/ui/doom-dashboard/banners/default.png")
+ '(markdown-command '+markdown-compile)
+ '(markdown-command-needs-filename nil)
+ '(markdown-live-preview-window-function 'markdown-live-preview-window-eww)
+ '(markdown-open-command "xdg-open")
+ '(markdown-split-window-direction 'right)
+ '(meghanada-completion-matcher "prefix")
+ '(meghanada-debug t)
+ '(meghanada-full-text-search-enable t)
+ '(meghanada-java-path "/usr/bin/java" t)
+ '(org-M-RET-may-split-line nil)
+ '(org-agenda-files
+   '("~/org/LEPES.org" "/home/ocramoi/org/Freela.org" "/home/ocramoi/org/rufus.org" "/home/ocramoi/org/Trabalhos.org" "/home/ocramoi/org/Pessoal.org"))
+ '(org-display-remote-inline-images 'download)
+ '(org-hide-emphasis-markers t)
+ '(org-hide-macro-markers t)
+ '(org-highlight-latex-and-related '(native latex script entities))
+ '(org-image-actual-width nil)
+ '(org-latex-prefer-user-labels t t)
+ '(org-num-face '(:inherit org-special-keyword :underline nil :weight bold) t)
+ '(org-startup-with-latex-preview t)
+ '(package-selected-packages
+   '(elpy company-arduino company-quickhelp python-cell fzf gemini-mode mips-mode undo-tree java-imports zenity-color-picker imenu-list pandoc-mode pandoc company-irony-c-headers eshell-toggle company-auctex auctex company-c-headers real-auto-save company-irony flycheck-irony irony-eldoc irony company))
+ '(projectile-git-submodule-command nil)
+ '(split-height-threshold nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(python-cell-highlight-face ((t (:inherit ##)))))
