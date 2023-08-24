070192f3c6c8a14899345bb33e995738daaf2fc6
Marco 'Ocramoi' Toledo <marcoantonioribeirodetoledo@gmail.com>
2023-08-24 01:16:14 -0300

    feature: ...

--- custom.el
+++ custom.el
@@ -10,10 +10,11 @@
  '(company-meghanada-insert-args t)
  '(fancy-splash-image
    "/home/ocramoi/.emacs.d/modules/ui/doom-dashboard/banners/default.png")
- '(markdown-command '+markdown-compile t)
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(markdown-command '+markdown-compile)
  '(markdown-command-needs-filename nil)
  '(markdown-live-preview-window-function 'markdown-live-preview-window-eww)
- '(markdown-open-command "xdg-open" t)
+ '(markdown-open-command "xdg-open")
  '(markdown-split-window-direction 'right)
  '(meghanada-completion-matcher "prefix")
  '(meghanada-debug t)
@@ -22,19 +23,20 @@
  '(org-M-RET-may-split-line nil)
  '(org-agenda-files
    '("/home/ocramoi/org/LEPES.org" "/home/ocramoi/org/Napala.org" "/home/ocramoi/org/Pessoal.org" "/home/ocramoi/org/Trabalhos.org" "/home/ocramoi/org/Freela.org" "/home/ocramoi/org/marcoantonioribeirodetoledo@gmail.com.org" "/home/ocramoi/org/mardt@usp.br.org"))
- '(org-directory "~/org" t)
+ '(org-directory "~/org")
  '(org-display-remote-inline-images 'download)
  '(org-hide-emphasis-markers t)
  '(org-hide-macro-markers t)
  '(org-highlight-latex-and-related '(native latex script entities))
  '(org-image-actual-width nil)
- '(org-latex-prefer-user-labels t t)
+ '(org-latex-prefer-user-labels t)
  '(org-num-face '(:inherit org-special-keyword :underline nil :weight bold) t)
  '(org-startup-with-latex-preview t)
  '(package-selected-packages
    '(elpy company-arduino company-quickhelp python-cell fzf gemini-mode mips-mode undo-tree java-imports zenity-color-picker imenu-list pandoc-mode pandoc company-irony-c-headers eshell-toggle company-auctex auctex company-c-headers real-auto-save company-irony flycheck-irony irony-eldoc irony company))
  '(projectile-git-submodule-command nil)
- '(split-height-threshold nil))
+ '(split-height-threshold nil)
+ '(warning-suppress-types '((org-element-cache) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
